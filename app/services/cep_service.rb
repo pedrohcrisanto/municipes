5e75f30c04e6f99d106dc3c0c7f131388d267913
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2021-02-12 08:55:41 -0300

    fix(service): change service class

--- app/services/cep_service.rb
+++ app/services/cep_service.rb
@@ -1,59 +1,61 @@
 class CepService
-    include HTTParty
-  
-    URL = "https://viacep.com.br/ws/"
-    FORMAT = "json"
-  
-    CepServiceData = Struct.new(:zip, :street, :complement, :neighborhood, :city, :uf, :ibge_code, keyword_init: true)
-  
-    attr_reader :data, :message
-  
-    def initialize(cep)
-      @cep = cep
-      @data = CepServiceData.new
-    end
-  
-    def find
-      response = call
-      if success?(response)
-        process_response(response)
-      else
-        process_error_message(response)
-      end
-    end
-  
-    private
-  
-    attr_reader :cep
-  
-    def call
-      self.class.get("#{URL}#{cep}/#{FORMAT}/")
-    end
-  
-    def process_response(response)
-      parse_cep_data(response_as_object(response.body))
-    end
+  include HTTParty
 
-    def parse_cep_data(response)
-      data.zip = response.cep
-      data.street = response.logradouro
-      data.complement = response.complemento
-      data.neighborhood = response.bairro
-      data.uf = response.uf
-      data.ibge_code = response.ibge
-      data.city = response.localidade
-      data
-    end
+  URL = "https://viacep.com.br/ws/"
+  FORMAT = "json"
+
+  CepServiceData = Struct.new(:zip, :street, :complement, :neighborhood, :city, :uf, :ibge_code, keyword_init: true)
+
+  attr_reader :data, :message
+
+  def initialize(cep)
+    @cep = cep
+    @data = CepServiceData.new
+  end
+
+  def find
+    response = call
   
-    def success?(response)
-      response.code == 200
+    if response.success? && response["erro"].nil?
+      process_response(response)
+      
+      @message = "Seus dados foram preenchidos corretamente."
+
+      true
+    else
+      process_error_message(response)
+
+      false
     end
-  
-    def process_error_message(response)
-      @message = "Algo deu errado!"
-    end
-  
-    def response_as_object(response)
-      JSON.parse(response, object_class: OpenStruct)
-    end
+  end
+
+  private
+
+  attr_reader :cep
+
+  def call
+    self.class.get("#{URL}#{cep}/#{FORMAT}/")
+  end
+
+  def process_response(response)
+    parse_cep_data(response_as_object(response.body))
+  end
+
+  def parse_cep_data(response)
+    data.zip = response.cep
+    data.street = response.logradouro
+    data.complement = response.complemento
+    data.neighborhood = response.bairro
+    data.uf = response.uf
+    data.ibge_code = response.ibge
+    data.city = response.localidade
+  end
+
+  def process_error_message(response)
+    @message = "CEP Inválido!" if response.code == 400 || response["erro"].present?
+  end
+
+  def response_as_object(response)
+    JSON.parse(response, object_class: OpenStruct)
+  end
 end
